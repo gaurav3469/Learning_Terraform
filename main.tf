0b887eb9afcb71f87f0a74cc9fb35a44e81a9649
gauravkumar <gauravkumar3469@gmail.com>
2023-07-02 19:44:14 +0800

    updated

--- main.tf
+++ main.tf
@@ -5,136 +5,135 @@ provider "google" {
   zone        = "us-central1-a"
 }
 
-
 module "testvm-1" {
-
   source = "./modules/module-1"
-  
 }
-resource "google_compute_network" "virtualnetwork" {
-  name = "my-network"
-}
+
+
+# resource "google_compute_network" "virtualnetwork" {
+#   name = "my-network"
+# }
 
 # Create two subnets within the virtual network
-resource "google_compute_subnetwork" "app_subnet" {
-  name          = "app-subnet"
-  region        = "us-central1"
-  network       = google_compute_network.my_network.self_link
-  ip_cidr_range = "10.0.1.0/24"
-}
+# resource "google_compute_subnetwork" "app_subnet" {
+#   name          = "app-subnet"
+#   region        = "us-central1"
+#   network       = google_compute_network.my_network.self_link
+#   ip_cidr_range = "10.0.1.0/24"
+# }
 
-resource "google_compute_subnetwork" "sql_subnet" {
-  name          = "sql-subnet"
-  region        = "us-central1"
-  network       = google_compute_network.my_network.self_link
-  ip_cidr_range = "10.0.2.0/24"
-}
+# resource "google_compute_subnetwork" "sql_subnet" {
+#   name          = "sql-subnet"
+#   region        = "us-central1"
+#   network       = google_compute_network.my_network.self_link
+#   ip_cidr_range = "10.0.2.0/24"
+# }
 
-# Create Windows application server
-resource "google_compute_instance" "app_server" {
-  name         = "app-server"
-  machine_type = "n1-standard-2"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "windows-2019"
-    }
-  }
-  network_interface {
-    network    = google_compute_network.my_network.self_link
-    subnetwork = google_compute_subnetwork.app_subnet.self_link
-    access_config {
-    }
-  }
-  metadata = {
-    windows-startup-script-ps1 = <<-EOT
-      # Set the username and password for the Windows VM
-      $username = "admin"
-      $password = "Password123"
+# # Create Windows application server
+# resource "google_compute_instance" "app_server" {
+#   name         = "app-server"
+#   machine_type = "n1-standard-2"
+#   zone         = "us-central1-a"
+#   boot_disk {
+#     initialize_params {
+#       image = "windows-2019"
+#     }
+#   }
+#   network_interface {
+#     network    = google_compute_network.my_network.self_link
+#     subnetwork = google_compute_subnetwork.app_subnet.self_link
+#     access_config {
+#     }
+#   }
+#   metadata = {
+#     windows-startup-script-ps1 = <<-EOT
+#       # Set the username and password for the Windows VM
+#       $username = "admin"
+#       $password = "Password123"
       
-      # Create a new local user
-      net user $username $password /add
+#       # Create a new local user
+#       net user $username $password /add
       
-      # Add the user to the administrators group
-      net localgroup administrators $username /add
+#       # Add the user to the administrators group
+#       net localgroup administrators $username /add
     
-     #Enable RDP
-      Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
-      Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#      #Enable RDP
+#       Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
+#       Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
     
-    EOT
-  }
-}
+#     EOT
+#   }
+# }
 
-# Create a virtual machine with SQL Server Database Engine 2019
-resource "google_compute_instance" "sql_server" {
-  name         = "sql-server"
-  machine_type = "n1-standard-4"
-  zone         = "us-central1-b"
-  boot_disk {
-    initialize_params {
-      image = "windows-2019"
-    }
-  }
-  network_interface {
-    network    = google_compute_network.my_network.self_link
-    subnetwork = google_compute_subnetwork.sql_subnet.self_link
-    access_config {
-    }
-  }
-  metadata = {
-    windows-startup-script-ps1 = <<-EOT
-      # Set the username and password for the Windows VM
-      $username = "sqladmin"
-      $password = "Password@123"
+# # Create a virtual machine with SQL Server Database Engine 2019
+# resource "google_compute_instance" "sql_server" {
+#   name         = "sql-server"
+#   machine_type = "n1-standard-4"
+#   zone         = "us-central1-b"
+#   boot_disk {
+#     initialize_params {
+#       image = "windows-2019"
+#     }
+#   }
+#   network_interface {
+#     network    = google_compute_network.my_network.self_link
+#     subnetwork = google_compute_subnetwork.sql_subnet.self_link
+#     access_config {
+#     }
+#   }
+#   metadata = {
+#     windows-startup-script-ps1 = <<-EOT
+#       # Set the username and password for the Windows VM
+#       $username = "sqladmin"
+#       $password = "Password@123"
       
-      # Create a new local user
-      net user $username $password /add
+#       # Create a new local user
+#       net user $username $password /add
       
-      # Add the user to the administrators group
-      net localgroup administrators $username /add
+#       # Add the user to the administrators group
+#       net localgroup administrators $username /add
       
-      # Download SQL Server installer
-      $url = "https://go.microsoft.com/fwlink/?linkid=866658"
-      $outfile = "C:\\SQLServerInstaller.exe"
-      Invoke-WebRequest $url -OutFile $outfile
+#       # Download SQL Server installer
+#       $url = "https://go.microsoft.com/fwlink/?linkid=866658"
+#       $outfile = "C:\\SQLServerInstaller.exe"
+#       Invoke-WebRequest $url -OutFile $outfile
       
-      # Install SQL Server Database Engine
-      Start-Process -FilePath $outfile -ArgumentList "/QS", "/ACTION=Install", "/FEATURES=SQLEngine", "/INSTANCENAME=MSSQLSERVER", "/SECURITYMODE=SQL", "/SAPWD=Password123" -Wait
+#       # Install SQL Server Database Engine
+#       Start-Process -FilePath $outfile -ArgumentList "/QS", "/ACTION=Install", "/FEATURES=SQLEngine", "/INSTANCENAME=MSSQLSERVER", "/SECURITYMODE=SQL", "/SAPWD=Password123" -Wait
      
-      #Enable RDP
-      Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
-      Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#       #Enable RDP
+#       Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
+#       Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
     
     
-    EOT
-  }
-}
+#     EOT
+#   }
+# }
 
-# Create a firewall rule to allow RDP access to the application server
-resource "google_compute_firewall" "app_server_rdp" {
-  name    = "app-server-rdp"
-  network = google_compute_network.my_network.self_link
+# # Create a firewall rule to allow RDP access to the application server
+# resource "google_compute_firewall" "app_server_rdp" {
+#   name    = "app-server-rdp"
+#   network = google_compute_network.my_network.self_link
 
-  allow {
-    protocol = "tcp"
-    ports    = ["3389"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["3389"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  //target_tags   = [google_compute_instance.app_server.tags]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#   //target_tags   = [google_compute_instance.app_server.tags]
+# }
 
-# Create a firewall rule to allow RDP access to the SQL Server
-resource "google_compute_firewall" "sql_server_rdp" {
-  name    = "sql-server-rdp"
-  network = google_compute_network.my_network.self_link
+# # Create a firewall rule to allow RDP access to the SQL Server
+# resource "google_compute_firewall" "sql_server_rdp" {
+#   name    = "sql-server-rdp"
+#   network = google_compute_network.my_network.self_link
 
-  allow {
-    protocol = "tcp"
-    ports    = ["3389"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["3389"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
- // target_tags   = [google_compute_instance.sql_server.tags]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#  // target_tags   = [google_compute_instance.sql_server.tags]
+# }
--- modules/module-1/resource.tf
+++ modules/module-1/resource.tf
@@ -1,27 +1,34 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
+
+resource "google_compute_instance" "windows_vm" {
+  name         = "my-windows-vm"
+  machine_type = "n1-standard-1"
   zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
+      image = "windows-2019"
     }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
   }
 
   network_interface {
     network = "default"
-
-   
   }
+
+  metadata_startup_script = <<-EOF
+    <powershell>
+    $secpasswd = ConvertTo-SecureString "Keppel@123" -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ("admin", $secpasswd)
+    Set-LocalUser -Name "admin" -Password $cred.Password -Verbose
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
+    EOF
+
+#   metadata {
+#     windows-startup-script-ps1 = <<-EOF
+#       $RdpPort = "3389"
+#       Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $RdpPort
+#       New-NetFirewallRule -DisplayName "Allow RDP" -Direction Inbound -Protocol TCP -LocalPort $RdpPort -Action Allow
+#     EOF
+#   }
+
+  tags = ["rdp"]
 }
+
